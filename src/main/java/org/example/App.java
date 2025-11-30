7b2d5dc3feed1d77c1bc05d7a7d9ab39f0d1db29
“[fares <[faresderouich4@gmail.com]>
2025-11-30 18:41:40 +0100

    Ajout d'une nouvelle feature dans App.java

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -3,6 +3,8 @@ package org.example;
 public class App {
     public static void main(String[] args) {
         System.out.println("Bonjour, projet_dev fonctionne !");
+        System.out.println("Bonjour, projet_dev fonctionne ! Version feature.");
+
     }
 
     public int addition(int a, int b) {
